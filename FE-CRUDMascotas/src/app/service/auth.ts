2ed86aca42b003a9b9d0e87ac75d88b608f98beb
Franco Rodriguez <franco.rodriguez.06@hotmail.com>
2026-05-08 23:25:11 -0300

    Navbar reactivo con logout

--- FE-CRUDMascotas/src/app/service/auth.ts
+++ FE-CRUDMascotas/src/app/service/auth.ts
@@ -1,9 +1,8 @@
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { LoginResponse } from '../interfaces/login-response';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { LoginRequest } from '../interfaces/login-request';
 
 @Injectable({
@@ -13,22 +12,30 @@ export class Auth {
 
   private baseUrl = `${environment.apiUrl}/Auth`;
 
+  // 🔥 estado reactivo
+  private loggedIn = new BehaviorSubject<boolean>(!!localStorage.getItem('token'));
+  isLoggedIn$ = this.loggedIn.asObservable();
+
   constructor(private http: HttpClient) {}
 
-  login(data: LoginRequest ): Observable<LoginResponse> {
+  login(data: LoginRequest): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${this.baseUrl}/login`, data);
   }
 
+  // 🔥 logout PRO
   logout() {
     localStorage.removeItem('token');
     localStorage.removeItem('usuario');
     localStorage.removeItem('rol');
+    this.loggedIn.next(false); // 👈 notifica a toda la app
   }
 
+  // 🔥 guardar sesión PRO
   saveSession(res: LoginResponse) {
     localStorage.setItem('token', res.token);
     localStorage.setItem('usuario', res.usuario);
     localStorage.setItem('rol', res.rol);
+    this.loggedIn.next(true); // 👈 notifica login
   }
 
   getToken(): string | null {
@@ -39,6 +46,7 @@ export class Auth {
     return localStorage.getItem('rol');
   }
 
+  // 🔹 lo dejamos (sirve para guards o checks rápidos)
   isLogged(): boolean {
     return !!localStorage.getItem('token');
   }
